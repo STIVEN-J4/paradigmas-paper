38eec876cd2a72baff0761e6fb14a7775096aa00
STIVEN-J4 <65369672+STIVEN-J4@users.noreply.github.com>
2020-06-04 15:40:00 -0500

    paradigmas

---
+++ Paradigmas.java
@@ -0,0 +1,81 @@
+
+package paradigmas;
+
+/**
+ *
+ * @author STIVEN
+ */
+import java.util.Scanner;
+public class Paradigmas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        Scanner objeto=new Scanner (System.in);
+
+    System.out.println("Estudio experimental y predicción de rendimiento "
+            + "y emisión en un motor SI que utiliza combustible alternativo "
+            + "con red neuronal artificial");
+
+    System.out.println("\n\nAtributos funcionales:");
+    String funcionales=objeto.nextLine();
+
+    System.out.println("\nAtributos no funcionales:");
+    String nofuncionales=objeto.nextLine();
+
+    System.out.println("\nObjetos del mundo real:");
+    String real=objeto.nextLine();
+
+    System.out.println("\nObjetos del mundo informatico");
+
+    System.out.println("\nObjeto 1");
+    String objeto1=objeto.nextLine();
+
+    System.out.println("\nAtributos");
+
+    System.out.println("Atributo_1:");
+    String atributo1=objeto.nextLine();
+
+    System.out.println(" Atributo_2:");
+    String atributo2=objeto.nextLine();
+
+    System.out.println(" Atributo_3:");
+    String atributo3=objeto.nextLine();
+
+    System.out.println("\nServicios");
+
+    System.out.println("Servicio_1:");
+    String servicio1=objeto.nextLine();
+
+    System.out.println(" Servicio_2:");
+    String servicio2=objeto.nextLine();
+
+    System.out.println(" Servicio_3:");
+    String servicio3=objeto.nextLine();
+
+
+    System.out.println("\nObjeto 2:");
+    String objeto2=objeto.nextLine();
+
+    System.out.println("\nAtributos");
+
+    System.out.println("Atributo_1:");
+    String atributo21=objeto.nextLine();
+
+    System.out.println(" Atributo_2:");
+    String atributo22=objeto.nextLine();
+
+    System.out.println("\nServicios");
+
+    System.out.println(" Servicio_1");
+    String servicio21=objeto.nextLine();
+
+    System.out.println(" Servicio_2");
+    String servicio22=objeto.nextLine();
+
+    }
+
+}
